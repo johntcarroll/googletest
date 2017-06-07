51ae550b63e363bc35f444f77bf6b9edbbaaed6d
John <john@John-Carroll.home>
2017-06-07 15:58:35 -0400

    first test

--- test.php
+++ test.php
@@ -1,3 +1,46 @@
 <?php
-
+session_start();
+error_reporting(E_ALL); ini_set('display_errors', 1);
 require_once 'google-api/src/Google/autoload.php';
+require_once '/home/ec2-user/google_creds.php';
+$gClient = new Google_Client();
+$gClient->setApplicationName('Login to WGACA-APPS');
+$gClient->setClientId($clientId);
+$gClient->setClientSecret($clientSecret);
+$gClient->setRedirectUri($redirectURL);
+
+$google_oauthV2 = new Google_Oauth2Service($gClient);
+
+if(isset($_GET['code'])){
+    $gClient->authenticate($_GET['code']);
+    $_SESSION['token'] = $gClient->getAccessToken();
+    header('Location: ' . filter_var($redirectURL, FILTER_SANITIZE_URL));
+}
+
+if (isset($_SESSION['token'])) {
+    $gClient->setAccessToken($_SESSION['token']);
+}
+
+if ($gClient->getAccessToken()) {
+    //Get user profile data from google
+    $gpUserProfile = $google_oauthV2->userinfo->get();
+
+
+    //Insert or update user data to the database
+    $gpUserData = array(
+        'oauth_provider'=> 'google',
+        'oauth_uid'     => $gpUserProfile['id'],
+        'first_name'    => $gpUserProfile['given_name'],
+        'last_name'     => $gpUserProfile['family_name'],
+        'email'         => $gpUserProfile['email'],
+        'gender'        => $gpUserProfile['gender'],
+        'locale'        => $gpUserProfile['locale'],
+        'picture'       => $gpUserProfile['picture'],
+        'link'          => $gpUserProfile['link']
+    );
+    print_r($gpUserData);
+} else {
+    $authUrl = $gClient->createAuthUrl();
+    $output = '<a href="'.filter_var($authUrl, FILTER_SANITIZE_URL).'"><img src="images/glogin.png" alt=""/></a>';
+}
+?>
